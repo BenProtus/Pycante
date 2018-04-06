88cf276fa8327009ea929dc81149e88b9b96a96d
abdulazizasiri <aasiri@lion.lmu.edu>
2018-04-06 14:51:59 -0700

    fix list[exp], and added grammar for it. Please check the grammar. it migh be wrong

--- ast/function-declaration.js
+++ ast/function-declaration.js
@@ -1,14 +1,9 @@
-const FunctionObject = require('./function-object');
-const Params = require('./parameter.js');
-
-// A function declaration binds a function object to a name.
-module.exports = class FunctionDeclaration {
-  constructor(id, params, body) {
-    this.id = id;
-    this.function = new FunctionObject(id, params, body);
-  }
-};
-
-// this.key = id;
-// this.params = params;
-// this.block = block;
+// const FunctionObject = require('./function-object');
+// const Params = require('./parameter.js');
+//
+// module.exports = class FunctionDeclaration {
+//   constructor(id, params, body) {
+//     this.id = id;
+//     this.function = new FunctionObject(id, params, body);
+//   }
+// };
