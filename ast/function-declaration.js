9939cc15fa933b3cc5bd6db38ab3962948debfda
abdulazizasiri <aasiri@lion.lmu.edu>
2018-04-06 17:46:56 -0700

    added changes Toal helped me with

--- ast/function-declaration.js
+++ ast/function-declaration.js
@@ -1,9 +1,13 @@
-// const FunctionObject = require('./function-object');
-// const Params = require('./parameter.js');
-//
-// module.exports = class FunctionDeclaration {
-//   constructor(id, params, body) {
-//     this.id = id;
-//     this.function = new FunctionObject(id, params, body);
-//   }
-// };
+const FunctionObject = require('./function-object');
+const Params = require('./parameter.js');
+
+module.exports = class FunctionDeclaration {
+  constructor(id, params, body) {
+    this.id = id;
+    this.function = new FunctionObject(id, params, body);
+  }
+
+  analyze() {
+
+  }
+};
--- ast/return-statement.js
+++ ast/return-statement.js
@@ -1,6 +1,19 @@
 module.exports = class ReturnStatement {
-  constructor(exp) {
-    this.expression = exp;
-    this.isReturn = true;
+  constructor(returnValue) {
+    this.returnValue = returnValue;
+  }
+
+  analyze(context) {
+    if (this.returnValue) {
+      this.returnValue.analyze(context);
+    }
+    context.assertInFunction('Return statement outside function');
+  }
+
+  optimize() {
+    if (this.returnValue) {
+      this.returnValue = this.returnValue.optimize();
+    }
+    return this;
   }
 };
