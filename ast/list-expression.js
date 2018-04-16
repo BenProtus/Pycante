10c7111796841004ff40589ec16cbb2da235b143
abdulazizasiri <aasiri@lion.lmu.edu>
2018-04-15 17:25:04 -0700

    list exp ast

--- ast/list-expression.js
+++ ast/list-expression.js
@@ -1,5 +1,22 @@
-module.exports = class SubscriptedExpression {
-  constructor(variable, subscript) {
-    Object.assign(this, { variable, subscript });
+
+const Type = require('./NamedType');
+
+module.exports = class List {
+  constructor(exp, lastExp) {
+    this.exp = exp;
+    this.lastExp = lastExp;
   }
+
+  analyze(context) {
+    this.lastExp.analyze(context);
+    for (let i = 0; i < this.exp.length; i += 1) {
+      this.exp[i].analyze(context);
+    }
+
+  }
+
+  optimize() {
+    return this;
+  }
+
 };
