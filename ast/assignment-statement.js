a8d9b1d0b5d2e87f020a646710f38d7486d291cc
abdulazizasiri <aasiri@lion.lmu.edu>
2018-04-26 18:50:02 -0700

    change assignmnet for ast

--- ast/assignment-statement.js
+++ ast/assignment-statement.js
@@ -1,19 +1,19 @@
 module.exports = class AssignmentStatement {
-  constructor(targets, sources) {
-    Object.assign(this, { targets, sources });
+  constructor(target, source) {
+    Object.assign(this, { target, source });
   }
 
   analyze(context) {
-    if (this.targets.length !== this.sources.length) {
-      throw new Error('Number of variables does not equal number of expressions');
-    }
-    this.sources.forEach(e => e.analyze(context));
-    this.targets.forEach(v => v.analyze(context));
+    // if (this.targets.length !== this.sources.length) {
+    //   throw new Error('Number of variables does not equal number of expressions');
+    // }
+    this.source.analyze(context);
+    this.target.analyze(context);
   }
 
   optimize() {
-    this.sources.forEach(e => e.optimize());
-    this.targets.forEach(v => v.optimize());
+    this.source.optimize();
+    this.target.optimize( );
     // Suggested: Turn self-assignments without side-effects to null
     return this;
   }
