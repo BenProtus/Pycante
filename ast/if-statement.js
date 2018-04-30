369f3e8fb5cf13f5f0a5b7ad669f45240e861618
abdulazizasiri <aasiri@lion.lmu.edu>
2018-04-30 15:24:03 -0700

    ifstat to check for asertInFunction

--- ast/if-statement.js
+++ ast/if-statement.js
@@ -1,5 +1,5 @@
 const Context = require('../semantics/context');
-
+const ReturnStatement = require("./return-statement");
 module.exports = class IfStatement {
   constructor(cases, alternate) {
     Object.assign(this, { cases, alternate });
@@ -10,6 +10,12 @@ module.exports = class IfStatement {
     if (this.alternate) {
       this.alternate.forEach(s => s.analyze(context.createChildContextForBlock()));
     }
+    
+  if (context.assertInFunction("if statement is not inside a function.") ) {
+
+  }
+
+
   }
   optimize() {
     this.cases.map(s => s.optimize()).filter(s => s !== null);
