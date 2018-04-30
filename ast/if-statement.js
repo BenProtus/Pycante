764edf058ab280533b42fe16e5a9d3999f2cbc3b
abdulazizasiri <aasiri@lion.lmu.edu>
2018-04-30 13:55:04 -0700

    added analyze and optimize, but it needs some modification

--- ast/if-statement.js
+++ ast/if-statement.js
@@ -1,13 +1,20 @@
+const Context = require('../semantics/context');
+
 module.exports = class IfStatement {
   constructor(cases, alternate) {
     Object.assign(this, { cases, alternate });
   }
 
-  analyze() { // eslint-disable-line class-methods-use-this
-    // Do something
+  analyze(Context) {
+    this.cases.forEach(c => c.analyze(context.createChildContextForBlock()));
+    if (this.alternate) {
+      this.alternate.forEach(s => s.analyze(context.createChildContextForBlock()));
+    }
   }
 
   optimize() {
+    this.cases.map(s => s.optimize()).filter(s => s !== null);
+    this.alternate = this.alternate ? this.alternate.optimize() : null;
     return this;
   }
 };
